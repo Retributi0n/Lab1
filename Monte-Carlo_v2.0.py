581b08cc117183afdc792b4074e0429e94d764e0
Retr0 <65489798+Retributi0n@users.noreply.github.com>
2021-02-20 13:05:17 +0300

    Monte-Carlo's method
    made under the editorship nart27

--- Monte-Carlo_v2.0.py
+++ Monte-Carlo_v2.0.py
@@ -1,43 +1,31 @@
 import matplotlib.pyplot as plt
 import random as rand
 
+IN = 0
+ALL = 4000
 
-IN=0
-OUT=10
+ax = plt.subplots()
 
-
-ax=plt.subplots()
-
-plt.fill([0,4,4,6,6,8,0,0,1,1,0],[0,0,2,2,0,0,8,5,5,4,4],'b')
+plt.fill([0, 4, 4, 6, 6, 8, 0, 0, 1, 1, 0], [0, 0, 2, 2, 0, 0, 8, 5, 5, 4, 4], 'b')
 
 plt.title("Monte-Carlo's method")
 plt.grid(True)
 
+for i in range(0, ALL):
+    xEx = ((rand.randrange(0, 8 * (10 ** 9)))) / (10 ** 9)  #diapazon X
+    yEx = ((rand.randrange(0, 8 * (10 ** 9)))) / (10 ** 9)  #diapazon Y
+    # print('x=',xEx,'y=',yEx)
 
-for i in range(0,OUT):
-    xEx=rand.randrange(0,8)
-    yEx=rand.randrange(0,8)
-    print('x=',xEx,'y=',yEx)
-
-
-    if xEx + yEx <= 8:
-        print('Point in range -x+8 ')
-        if xEx!=range(0,1) and yEx!=range(4,5):
-            print('Point in range -x+8 without small kvadrat')
-            if xEx!=range(4,6) and yEx!=range(0,2):
-                print('Point ...tra-ta-ta big kvadrat')
-                IN += 1
-                plt.scatter(xEx, yEx, c='g', zorder=10)
-        else:
-            plt.scatter(xEx, yEx, c='g', zorder=10)
+    if ((xEx + yEx <= 8) and not (
+            (xEx >= 0 and xEx <= 1 and yEx >= 4 and yEx <= 5) or (xEx >= 4 and xEx <= 6 and yEx >= 0 and yEx <= 2))):
+        IN += 1
+        plt.scatter(xEx, yEx, c='g', zorder=10)
     else:
         plt.scatter(xEx, yEx, c='r', zorder=10)
 
-
-S=(64*IN)/OUT
-print('S=',S)
-    # S fig= S kv * IN / H*W
+S = 64 * (IN / ALL)
+print('S=', S)
+# S fig= S kv * IN / H*W
 
 
 plt.show()
-
